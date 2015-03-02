00d12ab17d5330e83cea0ffaee074482b588f9a6
Juan Puig <juan@layer.com>
2015-03-02 11:24:37 -0800

    Add jitter based incremental backoff features

--- src/backoff.erl
+++ src/backoff.erl
@@ -1,10 +1,14 @@
 -module(backoff).
 -export([increment/1, increment/2]).
+-export([rand_increment/1, rand_increment/2]).
+-export([type/2]).
 -export([init/2, init/4,
          fire/1, get/1, succeed/1, fail/1]).
+
 -record(backoff, {start :: pos_integer(),
                   max :: pos_integer() | infinity,
                   current :: pos_integer(),
+                  type=normal :: normal | jitter,
                   value :: term(),
                   dest :: pid()}).
 
@@ -20,6 +24,21 @@ increment(N) when is_integer(N) -> N bsl 1.
     N :: pos_integer(),
     Max :: pos_integer().
 increment(N, Max) -> min(increment(N), Max).
+
+%% Just do the random increments by hand!
+%% Algorithm inspired in the Google HTTP Java client implementation of Class ExponentialBackOff.
+%% See: http://javadoc.google-http-java-client.googlecode.com/hg/1.18.0-rc/com/google/api/client/util/ExponentialBackOff.html
+-spec rand_increment(pos_integer()) -> pos_integer().
+rand_increment(N) ->
+    RandFactor = application:get_env(?MODULE, rand_factor, 0.5),
+    DefMultiplier = application:get_env(?MODULE, def_multiplier, 1.5),
+    Rand = 1 - RandFactor + random:uniform(),
+    erlang:round(increment(N) * DefMultiplier * Rand).
+
+-spec rand_increment(N, Max) -> pos_integer() when
+    N :: pos_integer(),
+    Max :: pos_integer().
+rand_increment(N, Max) -> min(rand_increment(N), Max).
 
 %% Increments + Timer support
 
@@ -52,16 +71,36 @@ fire(#backoff{current=Delay, value=Value, dest=Dest}) ->
 -spec get(backoff()) -> pos_integer().
 get(#backoff{current=Delay}) -> Delay.
 
+%% Swaps between the states of the backoff.
+-spec type(backoff(), normal | jitter) -> backoff().
+type(#backoff{}=B, jitter) ->
+    maybe_seed(),
+    B#backoff{type=jitter};
+type(#backoff{}=B, normal) ->
+    B#backoff{type=normal}.
+
 -spec fail(backoff()) -> {New::pos_integer(), backoff()}.
-fail(B=#backoff{current=Delay, max=infinity}) ->
+fail(B=#backoff{current=Delay, max=infinity, type=normal}) ->
     NewDelay = increment(Delay),
     {NewDelay, B#backoff{current=NewDelay}};
-fail(B=#backoff{current=Delay, max=Max}) ->
+fail(B=#backoff{current=Delay, max=Max, type=normal}) ->
     NewDelay = increment(Delay, Max),
+    {NewDelay, B#backoff{current=NewDelay}};
+fail(B=#backoff{current=Delay, max=infinity, type=jitter}) ->
+    NewDelay = rand_increment(Delay),
+    {NewDelay, B#backoff{current=NewDelay}};
+fail(B=#backoff{current=Delay, max=Max, type=jitter}) ->
+    NewDelay = rand_increment(Delay, Max),
     {NewDelay, B#backoff{current=NewDelay}}.
+
 
 -spec succeed(backoff()) -> {New::pos_integer(), backoff()}.
 succeed(B=#backoff{start=Start}) ->
     {Start, B#backoff{current=Start}}.
 
-
+maybe_seed() ->
+    case erlang:get(random_seed) of
+        undefined -> random:seed(erlang:now());
+        {X,X,X} -> random:seed(erlang:now());
+        _ -> ok
+    end.
